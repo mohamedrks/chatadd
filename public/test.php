df82d40c4e03e3a1c1f675eb0727be0d6eb38d83
mohamedrks <mohamedrks@gmail.com>
2015-05-06 00:52:29 +0530

    database configuration change

--- public/test.php
+++ public/test.php
@@ -7,3 +7,14 @@
  */
 
 echo 'Testing He Heee';
+
+include_once('config.php');
+//connect
+
+$con = mysqli_connect(DB_SERVER_IP,DB_SERVER_NAME,DB_SERVER_PASSWORD,DB_SERVER_USER_NAME);
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+} else {
+    echo "Succesfully connected to DB ";
+}
